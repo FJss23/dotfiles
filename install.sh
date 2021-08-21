1f5a46ea11f05eff5e5c71f0e24c611a2d43006d
FJss23 <uo252473@uniovi.es>
2021-08-22 00:02:43 +0200

    create a git dir for storing fit configuration

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ echo "Welcome to your script for recreating your enviroment"
 echo "Here are the list of packages that you should install"
 
 echo "1. Neovim"
+
+echo "example stow --target=${HOME} neovim  # will symlink relative to HOME (~)."
